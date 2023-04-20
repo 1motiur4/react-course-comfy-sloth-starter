0de49dc5a06ca239fc4ce4db748cb62f98d8755e
Motiur Muhith <1motiur4@gmail.com>
2023-04-20 16:46:07 -0400

    420 done

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -1,11 +1,30 @@
 import React from 'react'
 import styled from 'styled-components'
-import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs'
+import { BsStarFill, BsStarHalf, BsStar, BsSimFill } from 'react-icons/bs'
 const Stars = ({ stars, reviews }) => {
-  console.log(stars, reviews)
-  return <div className="">
+  const tempStars = Array.from({ length: 5 }, (_, index) => {
+    const number = index + 0.5;
+    return (
+      <span key={index}>
+        {stars >= index + 1 ? (
+          <BsStarFill />
+        ) : stars >= number ? (
+          <BsStarHalf />
+        ) : (
+          <BsStar />
+        )}
+      </span>
+    )
+  });
 
-  </div>
+  return <Wrapper>
+    <div className="stars">
+      {tempStars}
+    </div>
+    <p className="reviews">
+      ({reviews} customer reviews)
+    </p>
+  </Wrapper>
 }
 
 const Wrapper = styled.div`
@@ -23,3 +42,13 @@ const Wrapper = styled.div`
   margin-bottom: 0.5rem;
 `
 export default Stars
+
+{/* <span>
+{stars >= 1 ? (
+  <BsStarFill />
+) : stars >= 0.5 ? (
+  <BsStarHalf />
+) : (
+  <BsStar />
+)}
+</span> */}
